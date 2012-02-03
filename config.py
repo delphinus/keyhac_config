9f4d43394eec2b6b3e1a3d0f3661c459ed566adf
delphinus35 <delphinus@remora.cx>
2012-02-03 18:09:52 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,10 @@
+﻿# coding: utf-8
+# 設定開始
+import sys
+sys.path.append('C:/Dropbox/Programming/keyhac')
+import MyConfig
+
+def configure(km):
+    MyConfig.MyConfig(km)
+
+# vim:se et ts=4 sts=4 sw=4 fdm=marker:
